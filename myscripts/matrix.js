32cb52ba5dbfb29af21d8463c9648a7a9dc1aa0c
TuanDang <tdang21@uic.edu>
2016-04-05 15:18:37 -0500

    Add potential conflict matrix

--- myscripts/matrix.js
+++ myscripts/matrix.js
@@ -5,52 +5,71 @@ var mWidth = 800;
 var compareList = {};
 var array2D;
 var cellSize = 10;
-var cellSpacing = 1;
+var cellSpacing = 2;
+var cellMarginX = 50;
+var cellMarginY = 30;
 function drawMatrix(){
-
   array2D = []; 
-  var numCards = 
-  for (var i=0;i<tlinks.length;i++){
-    for (var j=0;j<tlinks.length;j++){
+  var numCards = tlinks.length;
+  for (var i=0;i<numCards;i++){
+    for (var j=0;j<numCards;j++){
       var cell = {};
-      var cell.row = tlinks[i];
-      var cell.rowId = i;
-      var cell.column = tlinks[j];
-      var cell.columnId = j;
+      cell.row = tlinks[i];
+      cell.rowId = i;
+      cell.column = tlinks[j];
+      cell.columnId = j;
       array2D.push(cell);
     }
   }  
 
   // Draw the matrix
-  svg3.selectAll(".cell").data(array2D).enter().append("rect")
-    .attr("class", "cells") 
+  svg3.selectAll(".cells").data(array2D).enter().append("rect")
+    .attr("class", function(d){
+      return "cells"+d.rowId+"__"+d.columnId;
+    }) 
     .attr("width", cellSize)
     .attr("height", cellSize)
-    .attr("x", function(d,i) {
-        var x0 = Math.floor(i / 100) % 10, x1 = Math.floor(i % 10);
-        return groupSpacing * x0 + (cellSpacing + cellSize) * (x1 + x0 * 10);
+    .attr("x", function(d) {
+        return cellMarginX+d.columnId*(cellSpacing + cellSize);
     })
-    .attr("y", function(i) {
-      var y0 = Math.floor(i / 1000), y1 = Math.floor(i % 100 / 10);
-      return groupSpacing * y0 + (cellSpacing + cellSize) * (y1 + y0 * 10);
+    .attr("y", function(d) {
+      return cellMarginY+d.rowId*(cellSpacing + cellSize);
     })
+    .style("fill-opacity", 0)
+    .style("stroke", "#aaa")
+    .style("stroke-width", 0.5);
+      
 
   // Download potential conflicts data  
   for (var i=0;i<tlinks.length;i++){
     for (var j=i+1;j<tlinks.length;j++){
       var cardI = tlinks[i].ref.ref;
       var cardJ = tlinks[j].ref.ref;
+      cardI.rowId = i;
+      cardJ.columnId = j;
       if (compareList[cardI._id+"__"+cardJ._id]==undefined){
         var postData = {
           cardA: cardI,
           cardB: cardJ
         }
-        d3.json('http://ccrg-data.evl.uic.edu/index-cards/api/IndexCards/compare')
+
+        var aaa = d3.json('http://ccrg-data.evl.uic.edu/index-cards/api/IndexCards/compare')
           .header('Content-Type', 'application/json')
           .post(JSON.stringify(postData))
           .on('load', function(d) { 
-            console.log("potentialConflict:"+d.response.comparsion.potentialConflict); 
-       //     debugger;
+            console.log("potentialConflict:"+d.response.comparsion.potentialConflict
+              +" A="+d.response.cardA.rowId+" B="+d.response.cardB.columnId+" score="+d.response.comparsion.score);
+            svg3.selectAll(".cells"+d.response.cardA.rowId+"__"+d.response.cardB.columnId)
+              .style("stroke", function(d2){
+                if (d.response.comparsion.potentialConflict)
+                  return "#f00";
+                else 
+                  return "#fff";
+              });
+              //.style("stroke-width", function(d2){
+              //  return d.response.comparsion.score;
+              //});
+            return d; 
           });
       }  
     }  
